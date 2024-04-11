772508589e889fdd80469540b9eab3828e768894
dr-scooby <hinurali@hotmail.com>
2024-04-11 00:07:58 -0700

    added comments

--- securedoc/src/main/java/com/jah/securedoc/SecuredocApplication.java
+++ securedoc/src/main/java/com/jah/securedoc/SecuredocApplication.java
@@ -2,7 +2,7 @@ package com.jah.securedoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// spring app
 @SpringBootApplication
 public class SecuredocApplication {
 
